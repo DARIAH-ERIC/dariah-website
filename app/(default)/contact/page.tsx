5d3def0496d98a00097fbe5c93ce21f5d86973fd
Stefan Probst <stefanprobst@posteo.de>
2025-12-18 20:25:55 +0100

    chore: add contact page placeholder

---
+++ app/(default)/contact/page.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
+import type { ReactNode } from "react";
+
+import { Main } from "@/app/(default)/_components/main";
+
+export async function generateMetadata(): Promise<Metadata> {
+	const t = await getTranslations("ContactPage");
+
+	const title = t("meta.title");
+
+	const metadata: Metadata = {
+		title,
+		// openGraph: {
+		// 	title,
+		// },
+	};
+
+	return metadata;
+}
+
+export default function ContactPage(): ReactNode {
+	const t = useTranslations("ContactPage");
+
+	return (
+		<Main className="container flex-1 px-8 py-12 xs:px-16">
+			<h1>{t("title")}</h1>
+		</Main>
+	);
+}
