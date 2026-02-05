4b3e7e62a8d5ec8774e79df317778c3d9b698d76
Stefan Probst <stefanprobst@posteo.de>
2026-02-05 20:39:52 +0100

    fix: try with keepalive disabled

--- lib/utils/request.ts
+++ lib/utils/request.ts
@@ -166,7 +166,7 @@ export async function request(url: URL | string, options: RequestOptions): Promi
 	return Result.tryPromise(
 		{
 			async try() {
-				const response = await fetch(request);
+				const response = await fetch(request, { keepalive: false });
 
 				if (!response.ok) {
 					throw new HttpError({ request, response });
