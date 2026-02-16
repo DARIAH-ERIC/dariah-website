e07c138ae100dc71a5d164605bdbdf2946fd6ef9
Bartosz Wiechec <bwiechec@man.poznan.pl>
2026-02-16 12:21:50 +0100

    feat/news-list: add new NewsCard variants, add news list based on mocked news

--- components/ui/news-card/news-card.tsx
+++ components/ui/news-card/news-card.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { cn } from "@acdh-oeaw/style-variants";
 import React, { type ReactNode } from "react";
 
@@ -13,29 +15,61 @@ interface NewsCardProps {
 	imageUrl: string;
 	linkUrl: string;
 	date: string;
-	variant: "featured" | "standard";
+	variant: "featured" | "standard" | "list-item" | "list-headline";
 }
 
 export function NewsCard(props: Readonly<NewsCardProps>): ReactNode {
 	const { variant, title, description, imageUrl, linkUrl, date } = props;
 
-	const imageWidth = variant === "featured" ? 755 : 361;
-	const imageHeight = variant === "featured" ? 339 : 244;
+	const getImageSizeForVariant = () => {
+		switch (variant) {
+			case "featured": {
+				return { imageWidth: 755, imageHeight: 339 };
+			}
+			case "standard": {
+				return { imageWidth: 361, imageHeight: 244 };
+			}
+			case "list-item": {
+				return { imageWidth: 362, imageHeight: 220 };
+			}
+			case "list-headline": {
+				return { imageWidth: 925, imageHeight: 431 };
+			}
+			default: {
+				return { imageWidth: 361, imageHeight: 244 };
+			}
+		}
+	};
+
+	const variantWithDescription = ["featured", "list-item", "list-headline"].includes(variant);
+	const isListVariant = ["list-item", "list-headline"].includes(variant);
+
+	const { imageWidth, imageHeight } = getImageSizeForVariant();
+
+	const containerVariants = {
+		featured: "w-188.75 h-147.25",
+		standard: "w-90.25 h-126",
+		"list-item": "w-188.25 h-55 gap-7",
+		"list-headline": "max-w-400 h-107.75 relative",
+	};
+
+	const imageWrapperVariants = {
+		featured: "w-188.75 h-84.75",
+		standard: "w-90.25 h-61",
+		"list-item": "w-90.5 h-55",
+		"list-headline": "w-231.25 h-107.75",
+	};
 
 	return (
 		<NavLink
 			className={cn(
-				"group flex flex-col cursor-pointer focus:outline-4 focus:outline-accent",
-				variant === "featured" ? "w-188.75 h-147.25" : "w-90.25 h-126",
+				"group flex cursor-pointer focus:outline-4 focus:outline-accent",
+				containerVariants[variant],
+				!isListVariant && "flex-col gap-0",
 			)}
 			href={linkUrl}
 		>
-			<div
-				className={cn(
-					"relative overflow-hidden",
-					variant === "featured" ? "w-188.75 h-84.75" : "w-90.25 h-61",
-				)}
-			>
+			<div className={cn("relative overflow-hidden", imageWrapperVariants[variant])}>
 				<Image
 					alt={title}
 					className={cn(
@@ -51,6 +85,7 @@ export function NewsCard(props: Readonly<NewsCardProps>): ReactNode {
 					className={cn(
 						"absolute bottom-0 bg-white flex gap-4 py-4.5",
 						variant === "featured" ? "px-8" : "px-4",
+						isListVariant && "hidden",
 					)}
 				>
 					<div className="flex gap-3.5 text-primary-500">
@@ -60,18 +95,48 @@ export function NewsCard(props: Readonly<NewsCardProps>): ReactNode {
 					<span className="text-gray-800">{date}</span>
 				</div>
 			</div>
-			<div className="flex flex-1 flex-col gap-4 pt-13 justify-between">
-				<Typography variant="h3">{title}</Typography>
-				{variant === "featured" && (
-					<Typography className="line-clamp-3" variant="regular">
+			<div
+				className={cn(
+					"flex flex-1 flex-col gap-4 justify-between",
+					!isListVariant && "pt-13",
+					variant === "list-item" && "gap-0! max-h-55 h-full",
+					variant === "list-headline" &&
+						"absolute right-0 z-10 max-w-197.25 bg-white justify-center p-6!",
+				)}
+			>
+				<div className="flex flex-col gap-2.5">
+					<div className={cn("flex gap-4", !isListVariant && "hidden")}>
+						<div
+							className={cn(
+								"flex gap-3.5 text-primary-500 items-center",
+								variant !== "list-item" && "hidden",
+							)}
+						>
+							<NewsIcon className="size-4" />
+							<Typography className="text-[16px] font-bold" variant="small">
+								{"NEWS"}
+							</Typography>
+						</div>
+						<span className="text-gray-800">{date}</span>
+					</div>
+					<Typography
+						className={cn(variant === "list-item" && "mb-3.5 line-clamp-2 h-13.5 text-[18px]")}
+						variant="h3"
+					>
+						{title}
+					</Typography>
+				</div>
+				{variantWithDescription && (
+					<Typography
+						className={cn("line-clamp-5", variant === "list-item" && "mb-2 line-clamp-3!")}
+						variant={variant === "list-item" ? "small" : "regular"}
+					>
 						{description}
 					</Typography>
 				)}
-				{}
-
 				<Typography
 					className={cn(
-						"font-semibold flex gap-2 items-center",
+						"font-semibold flex items-center",
 						"group-hover:text-primary group-hover:underline",
 						"group-focus:text-primary group-focus:underline",
 					)}
