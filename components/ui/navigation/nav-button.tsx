95987c6fe92148eba8c7bcb385ca1be3620da720
Bartosz Wiechec <bwiechec@man.poznan.pl>
2026-06-05 11:21:16 +0200

    fix/navigation-menu-shift: fix bottom padding on menu focus, fix footer shift on focus

--- components/ui/navigation/nav-button.tsx
+++ components/ui/navigation/nav-button.tsx
@@ -37,7 +37,7 @@ export function NavButton(props: Readonly<NavButtonProps>): ReactNode {
 				"group cursor-pointer bg-transparent p-2 flex gap-1 text-text-link-bg items-center uppercase [&>svg]:size-5 [&>svg]:fill-text-link-bg",
 				"hover:border-b-2 hover:pb-1.5 hover:border-b-primary hover:bg-white",
 				"data-focused:border-3 data-focused:border-primary data-focused:p-1.25 data-focused:outline-none",
-				"aria-expanded:border-b-3 aria-expanded:pb-1.5 aria-expanded:border-b-primary aria-expanded:bg-white",
+				"aria-expanded:border-b-3 aria-expanded:pb-1.25 aria-expanded:border-b-primary aria-expanded:bg-white",
 				"pressed:text-primary pressed:[&>svg]:fill-primary",
 				className,
 			)}
