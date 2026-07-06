5a8e7189bc79e5f0ff3af71fc07e30947db2aa74
Barbara Krautgartner <barbara.krautgartner@oeaw.ac.at>
2026-07-06 08:50:56 +0000

    chore: format

--- lib/search/schema/resources.ts
+++ lib/search/schema/resources.ts
@@ -92,25 +92,25 @@ export interface SearchCollectionItem {
 	source: string;
 	source_id: string;
 	type:
-	| "country"
-	| "document-or-policy"
-	| "event"
-	| "funding-call"
-	| "impact-case-study"
-	| "institution"
-	| "national-consortium"
-	| "news-item"
-	| "opportunity"
-	| "page"
-	| "person"
-	| "project"
-	| "spotlight-article"
-	| "working-group"
-	| "publication"
-	| "service"
-	| "software"
-	| "training-material"
-	| "workflow";
+		| "country"
+		| "document-or-policy"
+		| "event"
+		| "funding-call"
+		| "impact-case-study"
+		| "institution"
+		| "national-consortium"
+		| "news-item"
+		| "opportunity"
+		| "page"
+		| "person"
+		| "project"
+		| "spotlight-article"
+		| "working-group"
+		| "publication"
+		| "service"
+		| "software"
+		| "training-material"
+		| "workflow";
 	imported_at: number;
 	source_updated_at: number;
 }
