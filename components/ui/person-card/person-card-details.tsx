8852696dc0cc97bc499350eec3f11c0ab034b2c8
Stefan Probst <stefanprobst@posteo.de>
2026-07-20 10:29:59 +0200

    feat: add minimal person pages (#342)
    mainly so we can link to something when indexing persons in search
    index.
    
    closes #220

--- components/ui/person-card/person-card-details.tsx
+++ components/ui/person-card/person-card-details.tsx
@@ -1,14 +1,14 @@
 import type { JSONContent } from "@tiptap/core";
 import { useTranslations } from "next-intl";
-import { Fragment, type ReactNode } from "react";
+import type { ReactNode } from "react";
 
 import { Image } from "@/components/image";
 import { RichText } from "@/components/rich-text";
 import { EmailIcon } from "@/components/ui/icons/email";
 import { Link } from "@/components/ui/link/link";
+import { PersonPositions } from "@/components/ui/person-card/person-positions";
 import { Typography } from "@/components/ui/typography/typography";
 import type { Person } from "@/lib/data/api-client";
-import { sortUserPosition } from "@/utils/person-card.utils";
 
 interface PersonCardDetailsProps {
 	imageUrl?: string | null;
@@ -24,92 +24,6 @@ export function PersonCardDetails(props: Readonly<PersonCardDetailsProps>): Reac
 
 	const displayedImage = imageUrl ?? "/assets/images/person-placeholder.svg";
 
-	const sortedPosition = sortUserPosition(position);
-
-	const positionNames: Array<ReactNode> = sortedPosition
-		? sortedPosition.map((positionObj, index) => {
-				const { role, name, description, type } = positionObj;
-
-				if (role === "is_chair_of" && name.toLowerCase() === "board of directors")
-					return (
-						<Fragment key={`${role}_${name}_${type}`}>
-							{t("roles.is_president")}
-							{index < sortedPosition.length - 1 && ", "}
-						</Fragment>
-					);
-				if (role === "is_member_of" && name.toLowerCase() === "board of directors")
-					return (
-						<Fragment key={`${role}_${name}_${type}`}>
-							{t("roles.is_director")}
-							{index < sortedPosition.length - 1 && ", "}
-						</Fragment>
-					);
-
-				if (role === "is_chair_of" && type === "working_group")
-					return (
-						<Fragment key={`${role}_${name}_${type}`}>
-							{t("roles.is_chair_of_wg", {
-								name,
-							})}
-							{index < sortedPosition.length - 1 && ", "}
-						</Fragment>
-					);
-				if (role === "is_vice_chair_of" && type === "working_group")
-					return (
-						<Fragment key={`${role}_${name}_${type}`}>
-							{t("roles.is_vice_chair_of_wg", {
-								name,
-							})}
-							{index < sortedPosition.length - 1 && ", "}
-						</Fragment>
-					);
-
-				if (role === "is_member_of" && name.toLowerCase() === "dariah coordination office") {
-					if (description !== null)
-						return (
-							<Fragment key={`${role}_${name}_${type}`}>
-								{description}
-								{index < sortedPosition.length - 1 && ", "}
-							</Fragment>
-						);
-
-					return (
-						<Fragment key={`${role}_${name}_${type}`}>
-							{t.rich(`roles.capitalized_${role}`, {
-								name,
-								capitalizedSpan(chunks) {
-									return <span className="capitalize">{chunks}</span>;
-								},
-							})}
-							{index < sortedPosition.length - 1 && ", "}
-						</Fragment>
-					);
-				}
-
-				if ((role === "is_chair_of" || role === "is_vice_chair_of") && type === "governance_body")
-					return (
-						<Fragment key={`${role}_${name}_${type}`}>
-							{t.rich(`roles.capitalized_${role}`, {
-								name,
-								capitalizedSpan(chunks) {
-									return <span className="capitalize">{chunks}</span>;
-								},
-							})}
-							{index < sortedPosition.length - 1 && ", "}
-						</Fragment>
-					);
-
-				return (
-					<Fragment key={`${role}_${name}_${type}`}>
-						{t(`roles.${role}`, {
-							name,
-						})}
-						{index < sortedPosition.length - 1 && ", "}
-					</Fragment>
-				);
-			})
-		: [];
-
 	return (
 		<div className="flex gap-4">
 			<Image
@@ -121,7 +35,9 @@ export function PersonCardDetails(props: Readonly<PersonCardDetailsProps>): Reac
 			/>
 			<div className="flex flex-col gap-2 px-4">
 				<Typography variant="h5">{name}</Typography>
-				<Typography variant="regular">{positionNames}</Typography>
+				<Typography variant="regular">
+					<PersonPositions position={position} />
+				</Typography>
 				{email !== undefined && (
 					<Link
 						href={`mailto:${email}`}
---
+++ components/ui/person-card/person-positions.tsx
@@ -0,0 +1,107 @@
+import { useTranslations } from "next-intl";
+import { Fragment, type ReactNode } from "react";
+
+import type { Person } from "@/lib/data/api-client";
+import { sortUserPosition } from "@/utils/person-card.utils";
+
+interface PersonPositionProps {
+	position: Person["position"];
+}
+
+export function PersonPositions(props: Readonly<PersonPositionProps>): ReactNode {
+	const { position } = props;
+
+	const t = useTranslations("(default).PersonPositions");
+
+	const sortedPosition = sortUserPosition(position);
+
+	if (sortedPosition == null) {
+		return null;
+	}
+
+	return (
+		<Fragment>
+			{sortedPosition.map((positionObj, index) => {
+				const { role, name, description, type } = positionObj;
+
+				if (role === "is_chair_of" && name.toLowerCase() === "board of directors")
+					return (
+						<Fragment key={`${role}_${name}_${type}`}>
+							{t("roles.is_president")}
+							{index < sortedPosition.length - 1 && ", "}
+						</Fragment>
+					);
+				if (role === "is_member_of" && name.toLowerCase() === "board of directors")
+					return (
+						<Fragment key={`${role}_${name}_${type}`}>
+							{t("roles.is_director")}
+							{index < sortedPosition.length - 1 && ", "}
+						</Fragment>
+					);
+
+				if (role === "is_chair_of" && type === "working_group")
+					return (
+						<Fragment key={`${role}_${name}_${type}`}>
+							{t("roles.is_chair_of_wg", {
+								name,
+							})}
+							{index < sortedPosition.length - 1 && ", "}
+						</Fragment>
+					);
+				if (role === "is_vice_chair_of" && type === "working_group")
+					return (
+						<Fragment key={`${role}_${name}_${type}`}>
+							{t("roles.is_vice_chair_of_wg", {
+								name,
+							})}
+							{index < sortedPosition.length - 1 && ", "}
+						</Fragment>
+					);
+
+				if (role === "is_member_of" && name.toLowerCase() === "dariah coordination office") {
+					if (description !== null)
+						return (
+							<Fragment key={`${role}_${name}_${type}`}>
+								{description}
+								{index < sortedPosition.length - 1 && ", "}
+							</Fragment>
+						);
+
+					return (
+						<Fragment key={`${role}_${name}_${type}`}>
+							{t.rich(`roles.capitalized_${role}`, {
+								name,
+								capitalizedSpan(chunks) {
+									return <span className="capitalize">{chunks}</span>;
+								},
+							})}
+							{index < sortedPosition.length - 1 && ", "}
+						</Fragment>
+					);
+				}
+
+				if ((role === "is_chair_of" || role === "is_vice_chair_of") && type === "governance_body")
+					return (
+						<Fragment key={`${role}_${name}_${type}`}>
+							{t.rich(`roles.capitalized_${role}`, {
+								name,
+								capitalizedSpan(chunks) {
+									return <span className="capitalize">{chunks}</span>;
+								},
+							})}
+							{index < sortedPosition.length - 1 && ", "}
+						</Fragment>
+					);
+
+				return (
+					<Fragment key={`${role}_${name}_${type}`}>
+						{t(`roles.${role}`, {
+							name,
+						})}
+						{index < sortedPosition.length - 1 && ", "}
+					</Fragment>
+				);
+			})}
+		</Fragment>
+	);
+}
